11a587c586d76a2b7a033197717d7c8c047c52da
Eduarda Pinheiro-EDUAPINH <eduapinh@motorola.com>
2024-08-31 08:36:57 -0400

    Add project

---
+++ ApplePie/ApplePieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplePieApp.swift
+//  ApplePie
+//
+//  Created by Eduarda de Paula Paiva Pinheiro on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ApplePieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
